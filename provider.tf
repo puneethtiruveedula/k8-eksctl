ffb47f92cb06b3e88d2f72cae213b10b56f83ee2
puneeth tiruveedula <tiruveedulapuneeth3102@gmail.com>
2024-08-09 15:28:42 +0530

    k8

---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.48.0"
+    }
+  }
+  backend "s3" {
+    bucket = "puneeth-remote-state1"
+    key    = "expense-dev-k8-ws"
+    region = "us-east-1"
+    dynamodb_table = "puneeth-locking1"
+  }
+}
+
+#provide authentication here
+provider "aws" {
+  region = "us-east-1"
+}
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "sg_id" {
+  type        = string
+  default     = "sg-0275537a7daadb330"
+  description = "description"
+}
+
+variable "public_subnet_id" {
+    type = string
+    default = "subnet-0a5f6d6293341f595"
+}
