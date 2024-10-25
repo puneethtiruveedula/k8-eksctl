daeec5e3e7a5b85b2c3f94ce2ae11605d666cb35
puneethtiruveedula <tiruveedulapuneeth3102.com>
2024-10-25 15:05:43 +0530

    k8

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "sg_id" {
+  type        = string
+  default     = "sg-0c6609e8179fc11f6"
+  description = "description"
+}
+
+variable "public_subnet_id" {
+    type = string
+    default = "subnet-03aa9c0a9230d62e7"
+}
+
